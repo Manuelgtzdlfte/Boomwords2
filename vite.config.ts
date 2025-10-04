652e6f91f3e1fb32e98e461175bfb274a54c9626
Manuelgtzdlfte <manuelgutierrez821@gmail.com>
2025-10-04 02:17:41 -0600

    jejeje

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 export default defineConfig({
   plugins: [react()],
-  base: './',  // 🆕 Con B mayúscula
+  base: '/Boomwords2/', 
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
