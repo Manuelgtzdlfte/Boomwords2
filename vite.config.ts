fb0ba41aa6010a8482e15727773fba152c16a2e2
Manuelgtzdlfte <manuelgutierrez821@gmail.com>
2025-10-04 02:20:54 -0600

    Switch to HashRouter and relative base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   plugins: [react()],
-  base: '/Boomwords2/', 
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
+  base: './',  // 🆕 Base relativa
 })
