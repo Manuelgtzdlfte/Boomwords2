8bf38fff9946ebda574932142c650199650d4b0b
Manuelgtzdlfte <manuelgutierrez821@gmail.com>
2025-10-04 21:41:00 -0600

    a

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Boomwords2/',  // ✅ Verifica que sea EXACTAMENTE el nombre de tu repo
+  base: '/Boomwords2/',
   build: {
     outDir: 'dist',
-    sourcemap: false
-  }
+    sourcemap: false,
+    assetsDir: 'assets'
+  },
+  publicDir: 'public'
 })
